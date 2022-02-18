8179e731072fcfefe2e06b8b33a468c17f0f3e63
Karim <98668332+khadni@users.noreply.github.com>
2022-02-18 08:52:25 +0100

    First commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import { SearchBar } from '/Users/Karim/jammming/src/Components/SearchBar/SearchBar';
+
+function App() {
+  return (
+    <div>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <div className="App">
+        // Add a SearchBar component //
+        <div className="App-playlist">
+          // Add a SearchResults component //
+          // Add a Playlist component //
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
