4da9175834db819dba203499f3c1ae5ddf3906a1
Karim <98668332+khadni@users.noreply.github.com>
2022-02-18 10:01:04 +0100

    commit 2

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,20 +1,46 @@
-
+import React from 'react';
 import './App.css';
 import { SearchBar } from '/Users/Karim/jammming/src/Components/SearchBar/SearchBar';
+import { SearchResults } from '/Users/Karim/jammming/src/Components/SearchResults/SearchResults';
+import { Playlist } from '/Users/Karim/jammming/src/Components/Playlist/Playlist';
 
-function App() {
-  return (
-    <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
-      <div className="App">
-        // Add a SearchBar component //
-        <div className="App-playlist">
-          // Add a SearchResults component //
-          // Add a Playlist component //
+
+
+
+
+export class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      searchResults: [{
+        id: '1',
+        name: 'José',
+        artist: 'José le Chanteur',
+        album: 'José dernier Album'
+      },
+      {id: '1',
+      name: 'José',
+      artist: 'José le Chanteur',
+      album: 'José dernier Album'
+    }]
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults />
+            <Playlist />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+}
 }
 
 export default App;
---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './TrackList.css';
+import { Track } from '/Users/Karim/jammming/src/Components/Track/Track';
+
+export class TrackList extends React.Component{
+
+    render() {
+      return(
+        <div className="TrackList">
+          /// Search Results ////
+        </div>
+      )
+    }
+    
+}
